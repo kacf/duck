92496b9ab9e9bb62de39b6a3e4d718d35ffe62d2
D'Evangelista Nicolo <nicolo.deva@gmail.com>
2025-03-07 20:47:21 +0100

    formatted print for main

--- src/duckmind/main.py
+++ src/duckmind/main.py
@@ -2,5 +2,5 @@ from ai_manager import AIGenerator
 
 ai_client = AIGenerator()
 
-response = ai_client.generate_ddg("News of the day in Italy", model="o3-mini", has_search=True)
+response = ai_client.generate_ddg(f"""News of the day in Italy""", model="o3-mini", has_search=True)
 print(response)
