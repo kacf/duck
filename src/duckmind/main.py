70213300d8d7db5acf096e242f9c5f4442f0c853
Hritvik Ramteke <kacchan@outlook.in>
2025-09-09 20:59:51 +0530

    Update main.py

--- src/duckmind/main.py
+++ src/duckmind/main.py
@@ -3,7 +3,7 @@ from ai_manager import AIGenerator
 ai_client = AIGenerator()
 
 # Example usage of the AIGenerator using Duckai
-# response = ai_client.generate_ddg(f"""News of the day in Italy""", model="o3-mini", has_search=True)
+response = ai_client.generate_ddg(f"""News of the day in Italy""", model="o3-mini", has_search=True)
 
 # Example usage of the AIGenerator using Groq API (needs GROQ_API_KEY environment variable set)
 response = ai_client.generate(f"""News of the day in Italy""", model="qwen/qwen3-32b", has_search=True,
